a58954178e266b57a8233056b7b9a81b4366317b
Vagelis Kosmiadis <97461782+kosmiadis@users.noreply.github.com>
2026-06-18 16:36:53 +0300

    added card, item and sonner ui components

--- live-telemetry-dashboard/components/ui/typography.tsx
+++ live-telemetry-dashboard/components/ui/typography.tsx
@@ -7,7 +7,7 @@ export function H1 ({ children }: { children: ReactNode }) {
 }
 
 export function H2 ({ children }: { children: ReactNode }) {
-    return <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+    return <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
         {children}
     </h2>   
 }
